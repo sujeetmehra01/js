db75f337ffeb6006fdd90b0da0c9ea8f1b7d473e
SuJeet Mehra <82982529+sujeetmehra01@users.noreply.github.com>
2024-04-15 19:11:39 +0530

    variable updated

---
+++ index.js
@@ -0,0 +1,60 @@
+console.log("hello world");
+// console.log("hello world 2");
+// console.log(2 - 2);
+// console.log(3 - 4);
+// console.log(5 + 5);
+// console.log(6 * 6);
+// console.log(7 / 7);
+
+// variable (for comment ctrl + /)
+// 4 types
+// 0. no keyword automatically
+// 1. var
+// 2. let
+// 3. const
+// {
+//   name = "bharti";
+//   age = 2;
+//   number = 15;
+//   sum = age + number;
+//   console.log(number);
+//   console.log(name);
+//   console.log(sum);
+// }
+
+// name = "muskan";
+// number = 20;
+// console.log(name);
+// console.log(number);
+// var keyword
+// {
+//   var name = "bharti";
+//   var age = 2;
+//   var sum = age + name;
+//   console.log(name);
+//   console.log(sum);
+//   console.log(age);
+// }
+
+// name = "muskan";
+// age = 5;
+// console.log(name);
+
+// let keyword and const keyword
+// {
+//   let name = "muskan";
+//   let age = 5.2;
+//   let sum = name + age;
+//   console.log(sum);
+// }
+
+// const keywords
+{
+  const name = "muskan";
+  const age = 20;
+  const sum = name + age;
+  //   console.log(sum);
+  // name = "bharti";
+  // age = 25;
+  //   console.log(age);
+}
