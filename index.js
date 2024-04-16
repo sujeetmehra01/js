5b279bfed195d7878464d32c60875ef5c13ae88c
SuJeet Mehra <82982529+sujeetmehra01@users.noreply.github.com>
2024-04-16 12:42:05 +0530

    Operators updated

--- index.js
+++ index.js
@@ -49,12 +49,176 @@ console.log("hello world");
 // }
 
 // const keywords
-{
-  const name = "muskan";
-  const age = 20;
-  const sum = name + age;
-  //   console.log(sum);
-  // name = "bharti";
-  // age = 25;
-  //   console.log(age);
-}
+// {
+//   const name = "muskan";
+//   const age = 20;
+//   const sum = name + age;
+//   //   console.log(sum);
+//   // name = "bharti";
+//   // age = 25;
+//   //   console.log(age);
+// }
+
+// variable keywords case sensitive
+
+// var name;
+// var name;
+// let name1;
+// const name2;
+
+// letter, digits, underscores, and dollar sign
+// initial letter
+// $ or _ initial sign
+// let name;
+// let namE;
+
+// let var
+
+// var
+// // global variable
+// var n = 10;
+// {
+//   // block variables
+//   var n = 20;
+//   console.log(n);
+// }
+// console.log(n);
+
+// // let
+// // global variable
+// let n = 10;
+// n = 30;
+// {
+//   // block variables
+//   let n = 20;
+//   console.log(n);
+// }
+// console.log(n);
+
+// const
+// global variable
+// const n = 10;
+// {
+//   // block variables
+//   const n = 20;
+//   console.log(n);
+// }
+// console.log(n);
+
+// const PI = 3.14;
+// // PI = 3.15
+
+// PI = PI + 10;
+
+// operators
+// arithmetic
+// assignment
+// comparison
+// String
+// logical
+// bitwise
+// ternary
+// type
+
+// assignment  operator  (=)
+// let number;
+
+// number = "bharti" + (25 + 5);
+
+// console.log(number);
+
+// % modulus (division remainder)
+// let x = 20;
+// let y = 3;
+// %
+// console.log(x % y);
+
+// ++ increment
+// pre increment
+//  post increment
+// let x = 20;
+// console.log(x++); // 20
+
+// same
+// x++
+// x += 1;
+// x = x + 1;
+
+// console.log(x); //21
+// // x++; // 22
+// // x = x + 1; //23
+// console.log(x); //23
+
+// pre
+// let n = 40;
+
+// same
+// ++n;
+// n = 1 + n; //
+
+// console.log(++n); //41
+// console.log(n);
+// n += 1;
+// console.log(n); //
+
+// same
+// x = x+y
+// x+=y
+
+// camparison operators
+// == equal to (value check)
+// let num = 5;
+// let name = "5";
+
+// console.log(num == name); // true
+
+// === equal (value check) and equal (type check)
+// let num = 5;
+// let name = 5;
+
+// console.log(num === name); // true
+
+// != not equal (value check)
+// let num = 6;
+// let name = "6";
+
+// console.log(num != name); // true
+
+// !== not equal (value check) or equal (type check)
+// let num = "6";
+// let name = 7;
+
+// console.log(num !== name); // true
+
+// > greater than (value check)
+// let num = 7;
+// let name = "7";
+
+// console.log(num > name); // true
+
+// < less than (value check)
+// let num = 6;
+// let name = "7";
+
+// console.log(num < name); // true
+
+// >= greater than or equal to (value check)
+
+// let num = 7;
+// let name = "7";
+
+// console.log(num >= name); // true
+
+// <= less than or equal to (value check)
+// let num = 5;
+// let name = "6";
+
+// console.log(num <= name); // true
+
+// ? ternary operator
+let number = 20;
+let name = 20;
+
+// conditon ? true : false;
+
+number === name ? console.log("Hye") : console.log("Bye");
